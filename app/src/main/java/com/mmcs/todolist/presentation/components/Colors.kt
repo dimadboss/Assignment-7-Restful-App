1446f3a2171806aa2c395540920f9dc402218bb4
dmitry_drozdov <dmitry.drozdov@slcode.com>
2022-05-12 23:14:53 +0300

    base project

---
+++ app/src/main/java/com/mmcs/todolist/presentation/components/Colors.kt
@@ -0,0 +1,9 @@
+package com.mmcs.todolist.presentation.components
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val colorPrimary = Color(0xFFFFFFFF)
+    val colorOnPrimary = Color(0xFF252B30)
+    val colorSecondary = Color(0xFFFF11FF)
+}
